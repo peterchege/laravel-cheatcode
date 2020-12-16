aed72f6d09c21eae27ae3830ee9a703116b320f6
peter chege <peterchege442@gmail.com>
2020-12-16 20:05:21 +0300

    defining controllers in laravel 8

---
+++ blog/app/Http/Controllers/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class User extends Controller
+{
+    function index(){
+        echo "Hallo from controller";
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view("/about", 'about');
+Route::view("about", 'about');
+
+Route::get('users', [User::class, 'index']);
